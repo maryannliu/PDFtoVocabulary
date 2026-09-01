8de8a93ea4b66f0d30e64e9c36ea30219c5052a2
Mary Ann <maryannliu01@gmail.com>
2026-09-01 10:35:26 +1000

    filter out non english words

--- pdfToVocab.py
+++ pdfToVocab.py
@@ -1,9 +1,17 @@
 import pymupdf 
 import re 
+import nltk 
+from nltk.corpus import words
+
 doc = pymupdf.open('114P_English.pdf') 
 print("Pages:", len(doc)) 
 vocabulary = set() 
 
+# downloard nltk corpus once 
+# nltk.download('words')
+
+
+english_words = set(w.lower() for w in words.words())
 
 
 for page in doc:
@@ -17,4 +25,10 @@ for page in doc:
             part = re.sub(r'[.!?]', '', part)
             vocabulary.add(part)
 
-print(vocabulary)
+# Filter against NLTK's English word list 
+valid_vocab = { w.lower() for w in vocabulary if w.lower() in english_words }
+removed = vocabulary - valid_vocab
+
+print(f"Kept {len(valid_vocab)} words, removed {len(removed)} non-English/unknown tokens")
+print(removed)
+print(sorted(valid_vocab))
